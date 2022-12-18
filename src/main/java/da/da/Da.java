628d22848ce1394f3805ff23ef8e3281e7e5c254
KolyaYT <35967628+Kolya-YT@users.noreply.github.com>
2022-12-18 21:24:46 +0300

    Initial commit

---
+++ src/main/java/da/da/Da.java
@@ -0,0 +1,17 @@
+package da.da;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Da extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
